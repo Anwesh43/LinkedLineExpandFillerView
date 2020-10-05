95003112c927d43eed893048a00e554a595b634e
Anwesh43 <anweshthecool0@gmail.com>
2020-10-06 04:26:26 +0530

    adding LEFNode class

--- lineexpandfillerview/src/main/java/com/anwesh/uiprojects/lineexpandfillerview/LineExpandFilterView.kt
+++ lineexpandfillerview/src/main/java/com/anwesh/uiprojects/lineexpandfillerview/LineExpandFilterView.kt
@@ -124,4 +124,46 @@ class LineExpandFilterView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LEFNode(var i : Int, val state : State = State()) {
+
+
+        private var prev : LEFNode? = null
+        private var next : LEFNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LEFNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLEFNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUdpating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LEFNode {
+            var curr : LEFNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
